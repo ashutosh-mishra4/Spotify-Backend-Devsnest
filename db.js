78d52a8bcb15547f9e60c0d8f65db4762d74173c
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2023-01-16 20:37:57 +0530

    bug: replace quotes with back-ticks

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://ashutoshmishra:`${process.env.MONGO_PASSWORD}`@cluster0.zj9bepb.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = `mongodb+srv://ashutoshmishra:${process.env.MONGO_PASSWORD}@cluster0.zj9bepb.mongodb.net/?retryWrites=true&w=majority`
 
 const connectToMongo  = async () => {
     mongoose.connect(mongoURI, () => {
